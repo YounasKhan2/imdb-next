d794531b469a3891045f94abf60aa26bddbe2f49
YounasKhan2 <younaskk120@gmail.com>
2024-11-02 20:21:46 +0500

    dar mode

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,11 +1,9 @@
-'use client'
-import React from 'react';
 import Link from 'next/link';
 
 export default function MenuItem({ title, address, Icon }) {
   return (
     <Link href={address} className='hover:text-amber-500'>
-      <Icon className="text-2xl sm:hidden" /> 
+      <Icon className="text-2xl sm:hidden"/>
       <p className='uppercase hidden sm:inline text-sm'>{title}</p>
     </Link>
   );
